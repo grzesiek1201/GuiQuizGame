618d80623f10ade25afd23582d499a865d1b50f5
Grzegorz <138705337+grzesiek1201@users.noreply.github.com>
2024-03-22 20:12:43 +0100

    Update QuizTimers.py

--- TextQuizGame/QuizTimers.py
+++ TextQuizGame/QuizTimers.py
@@ -1,62 +1,47 @@
 import time
-from threading import Thread
+import threading
 
 
-class Timer:
+class Timers:
     def __init__(self):
-        self.start_time = None
-        self.time_answer = 30
-        self.paused_time = 0
+        self.player_time = 30
+        self.time_pause = False
         self.time_up = False
-        self.print_time = True
         self.timer_thread = None
 
-    def stop_timer(self):
-        if self.start_time is not None:
-            if self.paused_time > 0:
-                return self.paused_time
-            else:
-                return time.time() - self.start_time
-        else:
-            return 0
-
-    def start_timer(self):
-        self.time_up = False
-        self.start_time = time.time()
-        self.timer_thread = Thread(target=self.player_timer)
+    def start_time(self):
+        self.timer_thread = threading.Thread(target=self.count_down)
         self.timer_thread.start()
-        print("Timer started at:", self.start_time)
 
-    def pause_timer(self):
-        self.print_time = False
-        print("Timer paused at:", time.time(), "Print time:", self.print_time)
+    def count_down(self):
+        while self.player_time > 0:
+            if not self.time_pause:
+                time.sleep(1)
+                self.player_time -= 1
+            else:
+                time.sleep(1)
+        self.time_up = True
 
-    def resume_timer(self):
-        self.print_time = True
-        print("Timer resumed at:", time.time(), "Print time:", self.print_time)
+    def stop_time(self):
+        if self.timer_thread and self.timer_thread.is_alive():
+            self.timer_thread.join()
 
-    def restart_timer(self):
-        self.start_timer()
-        print("Timer restarted at:", self.start_time)
-        self.start_time = None
+    def help_pause_time(self):
+        self.time_pause = True
+
+    def help_resume_time(self):
+        self.time_pause = False
+
+    def help_add_time(self):
+        self.player_time += 30
+        print(f"+30 sec!. Current time left {self.player_time}")
+
+    def reset_timer(self):
+        self.player_time = 30
+        self.time_pause = False
         self.time_up = False
+        self.timer_thread = None
 
-    def player_timer(self):
-        while not self.time_up:
-            time.sleep(1)
-            if not self.print_time:
-                continue
-            remaining_time = self.time_answer - self.stop_timer()
-            if remaining_time <= 30 and remaining_time > 20:
-                print("30 seconds left")
-            elif remaining_time <= 20 and remaining_time > 10:
-                print("20 seconds left")
-            elif remaining_time <= 10 and remaining_time > 0:
-                print("10 seconds left")
-            elif remaining_time <= 0:
-                print("\nTime's up! You didn't answer the question.")
-                self.time_up = True
-
-    def add_timer(self):
-        self.time_answer += 30
-        print(f"+30 sec!. Current time left {self.time_answer}")
+    def reset_question_timer(self):
+        self.player_time = 30
+        print(f"Time reset{self.player_time}")
