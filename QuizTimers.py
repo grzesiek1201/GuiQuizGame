1a4ab210045eb286b0e07028b1e84ff76b433540
Grzegorz <138705337+grzesiek1201@users.noreply.github.com>
2024-04-28 21:43:46 +0200

    Update QuizTimers.py

--- QuizTimers.py
+++ QuizTimers.py
@@ -5,22 +5,21 @@ import threading
 class Timers:
     def __init__(self):
         self.player_time = 30
-        self.time_up = False
         self.timer_thread = None
+        self.timer_gui = None
 
     def start_time(self):
+        if self.timer_thread and self.timer_thread.is_alive():
+            self.timer_thread_stop = True
+            self.timer_thread.join()
+        self.timer_thread_stop = False
         self.timer_thread = threading.Thread(target=self.count_down)
         self.timer_thread.start()
 
     def count_down(self):
-        while self.player_time > 0:
+        while self.player_time > 0 and not getattr(self, 'timer_thread_stop', False):
             time.sleep(1)
             self.player_time -= 1
-            minutes = self.player_time // 60
-            seconds = self.player_time % 60
-            self.formatted_time = f"{minutes:02d}:{seconds:02d}"
-        else:
-            time.sleep(1)
 
     def stop_time(self):
         if self.timer_thread and self.timer_thread.is_alive():
@@ -29,11 +28,5 @@ class Timers:
     def help_add_time(self):
         self.player_time += 30
 
-    def reset_timer(self):
-        self.player_time = 30
-        self.time_up = False
-        self.timer_thread = None
-
     def reset_question_timer(self):
         self.player_time = 30
-
